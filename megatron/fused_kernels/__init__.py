0140eed18c2e17d021ee855fb6bfadf91094fa21
Samuel Antao <samuel.antao@amd.com>
2022-06-13 12:51:25 -0500

    Use --version nvcc to work with nvhpc suite.

--- megatron/fused_kernels/__init__.py
+++ megatron/fused_kernels/__init__.py
@@ -96,7 +96,7 @@ def load(args):
 
 
 def _get_cuda_bare_metal_version(cuda_dir):
-    raw_output = subprocess.check_output([cuda_dir + "/bin/nvcc", "-V"],
+    raw_output = subprocess.check_output([cuda_dir + "/bin/nvcc", "--version"],
                                          universal_newlines=True)
     output = raw_output.split()
     release_idx = output.index("release") + 1
